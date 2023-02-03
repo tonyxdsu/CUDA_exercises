4fa784c06a9b9530ea530a6ba3c1a5220ca28e46
tonyxdsu <tonyxdsu@gmail.com>
2023-02-02 23:44:14 -0800

    tabs to spaces

--- histogram_equalization/src/ppm_image.cpp
+++ histogram_equalization/src/ppm_image.cpp
@@ -18,67 +18,67 @@ PPMImage::PPMImage(unsigned int width, unsigned int height) : width(width), heig
 PPMImage::PPMImage(const std::string& fileName) {
     std::ifstream ifs(fileName, std::ifstream::in | std::ifstream::binary);
 
-	if (!ifs.is_open()) {
-		std::cerr << "Unable to open file " << fileName << std::endl;
-	}
+    if (!ifs.is_open()) {
+        std::cerr << "Unable to open file " << fileName << std::endl;
+    }
 
-	// first line
-	char curLine[64];
-	ifs.getline(curLine, 64);
+    // first line
+    char curLine[64];
+    ifs.getline(curLine, 64);
 
-	if (strcmp(curLine, "P6") != 0) {
-		std::cerr << "No P6 tag found in " << fileName << ", got: " << curLine << std::endl;
-	}
+    if (strcmp(curLine, "P6") != 0) {
+        std::cerr << "No P6 tag found in " << fileName << ", got: " << curLine << std::endl;
+    }
 
-	// skip comments
-	ifs.getline(curLine, 64);
-	while (curLine[0] == '#') {
-		ifs.getline(curLine, 64);
-	}
+    // skip comments
+    ifs.getline(curLine, 64);
+    while (curLine[0] == '#') {
+        ifs.getline(curLine, 64);
+    }
 
-	// read dimensions
-	sscanf(curLine, "%d %d", &width, &height);
+    // read dimensions
+    sscanf(curLine, "%d %d", &width, &height);
 
-	// read maximum pixel value
-	ifs.getline(curLine, 64);
-	// sscanf(curLine, "%d", &maxPixelValue); // don't think we need this
+    // read maximum pixel value
+    ifs.getline(curLine, 64);
+    // sscanf(curLine, "%d", &maxPixelValue); // don't think we need this
 
-	// // allocate flattened contiguous memory that is still able to be referenced with A[y][x]
-	// rA = new unsigned char*[height];
-	// gA = new unsigned char*[height];
-	// bA = new unsigned char*[height];
+    // // allocate flattened contiguous memory that is still able to be referenced with A[y][x]
+    // rA = new unsigned char*[height];
+    // gA = new unsigned char*[height];
+    // bA = new unsigned char*[height];
 
-	// // A[0] still hold the entire flattened array so cudamemcpy is happy
-	// rA[0] = new unsigned char[height * width];
-	// gA[0] = new unsigned char[height * width];
-	// bA[0] = new unsigned char[height * width];
+    // // A[0] still hold the entire flattened array so cudamemcpy is happy
+    // rA[0] = new unsigned char[height * width];
+    // gA[0] = new unsigned char[height * width];
+    // bA[0] = new unsigned char[height * width];
 
-	// // point indices A[1] to A[height - 1] to the start of each row so A[y][x] still works
-	// for (int y = 1; y < height; y++) {
-	// 	rA[y] = rA[y - 1] + width;
-	// 	gA[y] = gA[y - 1] + width;
-	// 	bA[y] = bA[y - 1] + width;
-	// }
+    // // point indices A[1] to A[height - 1] to the start of each row so A[y][x] still works
+    // for (int y = 1; y < height; y++) {
+    // 	rA[y] = rA[y - 1] + width;
+    // 	gA[y] = gA[y - 1] + width;
+    // 	bA[y] = bA[y - 1] + width;
+    // }
 
     cudaMallocManaged(&data, width * height * 3 * sizeof(unsigned char));
     cudaDeviceSynchronize();
     
     char tempChar;
-	for (int y = 0; y < height; y++) {
-		for (int x = 0; x < width; x++) {
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
             int index = (y * width + x) * 3;
-			ifs.read(&tempChar, 1);
-			data[index] = tempChar;
+            ifs.read(&tempChar, 1);
+            data[index] = tempChar;
 
-			ifs.read(&tempChar, 1);
-			data[index + 1] = tempChar;
+            ifs.read(&tempChar, 1);
+            data[index + 1] = tempChar;
 
-			ifs.read(&tempChar, 1);
-			data[index + 2] = tempChar;
-		}
-	}
+            ifs.read(&tempChar, 1);
+            data[index + 2] = tempChar;
+        }
+    }
 
-	ifs.close();
+    ifs.close();
 }
 
 PPMImage::~PPMImage() {
