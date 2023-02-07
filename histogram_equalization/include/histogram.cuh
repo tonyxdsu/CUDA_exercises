d1439e32544fe505cd0d9b80b8f2bc3a8c86d9ec
tonyxdsu <tonyxdsu@gmail.com>
2023-02-07 00:54:44 -0800

    simple kernel with templated tensor1D

--- histogram_equalization/include/histogram.cuh
+++ histogram_equalization/include/histogram.cuh
@@ -2,14 +2,18 @@
 #define HISTOGRAM_CUH
 
 #include "ppm_image.h"
+#include "tensor1D.h"
 
 #define BLOCK_SIZE_HISTOGRAM 256
 
+
 /**
- * @brief Creates a histogram of the frequency of value occurances in input
+ * @brief Creates a histogram of the frequency of value occurances in input.
  * @param input Assumed to be allocated on unified memory.
- * @return 
+ * @param size Number of elements in input.
+ * @param maxInputVal Maximum value in input. Used to determine number of bins in histogram.
+ * @return returns a pointer to the histogram. Allocated on unified memory and must be freed by caller.
  */
-unsigned int* createHistogram(unsigned char* input, unsigned int size, unsigned int maxInputVal);
+Tensor1D<unsigned int>* createHistogram(Tensor1D<unsigned char>* input, unsigned int maxInputVal);
 
 #endif
--- histogram_equalization/src/histogram.cu
+++ histogram_equalization/src/histogram.cu
@@ -2,6 +2,7 @@
 #include "device_launch_parameters.h"
 
 #include "../include/histogram.cuh"
+#include "../include/tensor1D.h"
 
 /**
  * blockDim.x >= numBins since first numBins threads in each block will write to global memory
@@ -9,38 +10,41 @@
  * TODO try textbook's algorithm instead where they use total number of threads in kernel << length
  * This version has too many accesses to shared memory; textbook has each thread calculate multiple elements and update an accumulator in register memory, then write once to shared memory
 */
-__global__ void createHistogramKernelSimple(unsigned char* input, unsigned int length, unsigned int numBins, unsigned int* histogramOutput) {
+__global__ void createHistogramKernelSimple(Tensor1D<unsigned char>* input, unsigned int numBins, Tensor1D<unsigned int>* histogramOutput) {
     int index = blockDim.x * blockIdx.x + threadIdx.x;
-    if (index >= length) { 
-        return;
-    }
+    
+
 
     // TODO compared vs statically allocated size since we're just dealing with images maxVal == 255
     extern __shared__ unsigned int histogramShared[];
 
-    atomicAdd(&(histogramShared[input[index]]), 1);
+    if (index < input->totalSize) { 
+        atomicAdd(&(histogramShared[input->elements[index]]), 1);
+    }
 
     __syncthreads();
 
     // first numBins threads of each block will write to global memory
     if (threadIdx.x < numBins) {
-        atomicAdd(&(histogramOutput[threadIdx.x]), histogramShared[threadIdx.x]);
+        atomicAdd(&(histogramOutput->elements[threadIdx.x]), histogramShared[threadIdx.x]);
     }
 }
 
-unsigned int* createHistogram(unsigned char* input, unsigned int size, unsigned int maxInputVal) {
-    unsigned int* histogramOutput = 0;
-    cudaMallocManaged(&histogramOutput, maxInputVal * sizeof(unsigned int));
+Tensor1D<unsigned int>* createHistogram(Tensor1D<unsigned char>* input, unsigned int maxInputVal) {
+    Tensor1D<unsigned int>* histogramOutput = new Tensor1D<unsigned int>(maxInputVal);
 
-    unsigned int gridXDim = size / BLOCK_SIZE_HISTOGRAM;
-    if (size % BLOCK_SIZE_HISTOGRAM != 0) {
+    unsigned int gridXDim = input->totalSize / BLOCK_SIZE_HISTOGRAM;
+    if (input->totalSize % BLOCK_SIZE_HISTOGRAM != 0) {
         gridXDim++;
     }
 
-    createHistogramKernelSimple<<<gridXDim, BLOCK_SIZE_HISTOGRAM, maxInputVal * sizeof(unsigned int)>>>(input, size, maxInputVal, histogramOutput);
+    createHistogramKernelSimple<<<gridXDim, BLOCK_SIZE_HISTOGRAM, maxInputVal * sizeof(unsigned int)>>>(input, maxInputVal, histogramOutput);
 
     cudaDeviceSynchronize();
 
     return histogramOutput;
 }
+
+
+
 
